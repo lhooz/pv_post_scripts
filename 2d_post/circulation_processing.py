79624143e8c79abdb3a901d774e4bf8db6b62913
hao <hao.lee0019@gmail.com>
2020-08-14 17:26:12 +0100

    added circulation filter and time series processing

--- 2d_post/circulation_processing.py
+++ 2d_post/circulation_processing.py
@@ -4,98 +4,72 @@ import os
 
 import numpy as np
 from scipy import ndimage
+from cprocessing_functions import field_plot, vortices_processing
 
-from cprocessing_functions import (field_plot, geometry_filter, grid_vorz,
-                                   read_sfield, read_wgeo, threshold_filter,
-                                   vorz_processing)
+window = [-6, 4, -4, 4]
+resolution = [500, 400]
+threshold_q = 0.002
+threshold_vorz = 0.02
+threshold_circulation = 0.02
+wbound_radius = 0
+
+data_time_increment = 2e-2
 
 cwd = os.getcwd()
 vorz_folder = os.path.join(cwd, 'vorz_data')
 q_folder = os.path.join(cwd, 'q_data')
 wgeo_folder = os.path.join(cwd, 'wgeo_data')
 
-# for filei in os.listdir(data_folder):
-# os.path.join(data_folder, filei)
+oimage_folder = os.path.join(cwd, 'oimages')
+if os.path.exists(oimage_folder):
+    shutil.rmtree(oimage_folder)
+os.mkdir(oimage_folder)
+#----------------------------------------
+vortex_0 = '0,0,0,0'
+with open('mpv', 'w') as f:
+    f.write("%s\n" % vortex_0)
+with open('npv', 'w') as f:
+    f.write("%s\n" % vortex_0)
+#----------------------------------------
+mp_vortex = [[0, 0, 0, 0]]
+np_vortex = [[0, 0, 0, 0]]
+for ti in range(1, len(os.listdir(q_folder))):
+    # for ti in range(1):
+    time_instance = str(ti).zfill(4)
+    timei = ti * data_time_increment
 
-time_instance = '0072'
-vorz_data_file = os.path.join(vorz_folder, 'vorz_' + time_instance + '.csv')
-q_data_file = os.path.join(q_folder, 'q_' + time_instance + '.csv')
-wgeo_data_file = os.path.join(wgeo_folder, 'wgeo_' + time_instance + '.csv')
+    vorz_data_file = os.path.join(vorz_folder,
+                                  'vorz_' + time_instance + '.csv')
+    q_data_file = os.path.join(q_folder, 'q_' + time_instance + '.csv')
+    wgeo_data_file = os.path.join(wgeo_folder,
+                                  'wgeo_' + time_instance + '.csv')
 
-vor_array = read_sfield(vorz_data_file)
-q_array = read_sfield(q_data_file)
-wgeo_array = read_wgeo(wgeo_data_file)
+    oimage_file = os.path.join(oimage_folder,
+                               'anime_' + time_instance + '.png')
 
-window = [-6, 4, -4, 4]
-resolution = [1000, 800]
-threshold_q = 0.002
-threshold_vorz = 0.02
-threshold_circulation = 0.02
-wbound_radius = 0
-# ----------data filtering (pre processing)----------------------
-grid_x, grid_y, grid_vz = grid_vorz(window, resolution, vor_array)
-grid_x, grid_y, grid_q = grid_vorz(window, resolution, q_array)
+    files_dir = [vorz_data_file, q_data_file, wgeo_data_file]
+    res_parameters = [window, resolution]
+    threshold_parameters = [
+        threshold_q, threshold_vorz, threshold_circulation, wbound_radius
+    ]
 
-g_filter = geometry_filter(grid_x, grid_y, wgeo_array, wbound_radius)
+    sorted_vortices, image_vortices, vz_field_filtered = vortices_processing(
+        files_dir, res_parameters, threshold_parameters)
 
-t_filter_q = threshold_filter(grid_q, threshold_q, 'q')
-t_filter_vorz = threshold_filter(grid_vz, threshold_vorz, 'vorticity')
-t_filter = np.multiply(t_filter_q, t_filter_vorz)
-# t_filter = t_filter_q
-# t_filter = t_filter_vorz
+    pvi = sorted_vortices[0][0]
+    nvi = sorted_vortices[1][0]
+    mp_vortex.append([timei, pvi[0], pvi[1], pvi[2]])
+    np_vortex.append([timei, nvi[0], nvi[1], nvi[2]])
 
-vorz_filter = np.multiply(g_filter, t_filter)
+    # print(sorted_vortices[1])
+    field_plot(window, vz_field_filtered[3], image_vortices[0], oimage_file,
+               'save')
 
-grid_vz_filtered = vorz_processing(grid_vz, vorz_filter)
-
-# field_plot(window, grid_vz, grid_vz_filtered)
-# field_plot(window, grid_vz, vorz_filter)
-#----------------------------------------------------------------
-
-#-----------vortices processing------------------------
-s = ndimage.generate_binary_structure(2, 2)
-vorz_l = ndimage.label(grid_vz_filtered, structure=s)
-# print(vorz_l[1])
-#-----------locations----------------
-pixel_locations = ndimage.measurements.center_of_mass(
-    grid_vz_filtered, vorz_l[0], [x for x in range(1, vorz_l[1] + 1)])
-# print(pixel_locations)
-dx = (window[1] - window[0]) / resolution[0]
-dy = (window[3] - window[2]) / resolution[1]
-vorz_locations = []
-for loci in pixel_locations:
-    v_locix = window[0] + loci[0] * dx
-    v_lociy = window[2] + loci[1] * dy
-    vorz_locations.append([v_locix, v_lociy])
-
-vorz_locations = np.array(vorz_locations)
-#-----------circulations----------------
-pixel_sum = ndimage.sum(grid_vz_filtered, vorz_l[0],
-                        [x for x in range(1, vorz_l[1] + 1)])
-circulations = []
-for i in range(len(pixel_sum)):
-    circulations.append([pixel_sum[i] * dx * dy, i + 1])
-circulations = np.array(circulations)
-
-vortices = np.array(
-    [[loc[0], loc[1], vz[0], vz[1]]
-     for loc, vz in zip(vorz_locations, circulations)
-     if vz[0] >= threshold_circulation * np.amax(circulations[:, 0])
-     or vz[0] <= threshold_circulation * -np.amax(-circulations[:, 0])])
-pvortices = np.array(
-    [[loc[0], loc[1], vz[0], vz[1]]
-     for loc, vz in zip(vorz_locations, circulations)
-     if vz[0] >= threshold_circulation * np.amax(circulations[:, 0])])
-nvortices = np.array(
-    [[loc[0], loc[1], vz[0], vz[1]]
-     for loc, vz in zip(vorz_locations, circulations)
-     if vz[0] <= threshold_circulation * -np.amax(-circulations[:, 0])])
-#-------------------------------------------------------------------------
-show_vortices = nvortices
-print(show_vortices)
-
-circulation_filter = vorz_filter * 0
-for vortexi in show_vortices:
-    circulation_filter += (vorz_l[0] == vortexi[3]) * 1
-
-field_plot(window, grid_vz, circulation_filter)
+    pvi = [str(timei), str(pvi[0]), str(pvi[1]), str(pvi[2])]
+    pvi = ','.join(pvi)
+    nvi = [str(timei), str(nvi[0]), str(nvi[1]), str(nvi[2])]
+    nvi = ','.join(nvi)
+    with open('mpv', 'a') as f:
+        f.write("%s\n" % pvi)
+    with open('npv', 'a') as f:
+        f.write("%s\n" % nvi)
--- 2d_post/cprocessing_functions.py
+++ 2d_post/cprocessing_functions.py
@@ -2,10 +2,12 @@
 
 import os
 import csv
+import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.path as mpltPath
 import scipy.interpolate
+from scipy import ndimage
 
 
 def read_sfield(field_data_file):
@@ -118,7 +120,7 @@ def vorz_processing(grid_vz, final_filter):
     return grid_vz_final
 
 
-def field_plot(window, grid_data, grid_data_processed):
+def field_plot(window, grid_data, grid_data_processed, oimage_file, mode):
     """plot field data"""
     plt.subplot(121)
     plt.imshow(grid_data.T,
@@ -134,4 +136,113 @@ def field_plot(window, grid_data, grid_data_processed):
     plt.title('vorz_processed')
     # plt.gcf().set_size_inches(6, 6)
 
-    plt.show()
+    if mode == 'save':
+        plt.savefig(oimage_file)
+    elif mode == 'show':
+        plt.show()
+
+
+def vortices_processing(files_dir, res_parameters, threshold_parameters):
+    """main function for vortices processing"""
+    vorz_data_file = files_dir[0]
+    q_data_file = files_dir[1]
+    wgeo_data_file = files_dir[2]
+
+    window = res_parameters[0]
+    resolution = res_parameters[1]
+
+    threshold_q = threshold_parameters[0]
+    threshold_vorz = threshold_parameters[1]
+    threshold_circulation = threshold_parameters[2]
+    wbound_radius = threshold_parameters[3]
+
+    vor_array = read_sfield(vorz_data_file)
+    q_array = read_sfield(q_data_file)
+    wgeo_array = read_wgeo(wgeo_data_file)
+    # ----------data filtering (pre processing)----------------------
+    grid_x, grid_y, grid_vz = grid_vorz(window, resolution, vor_array)
+    grid_x, grid_y, grid_q = grid_vorz(window, resolution, q_array)
+
+    g_filter = geometry_filter(grid_x, grid_y, wgeo_array, wbound_radius)
+
+    t_filter_q = threshold_filter(grid_q, threshold_q, 'q')
+    t_filter_vorz = threshold_filter(grid_vz, threshold_vorz, 'vorticity')
+    t_filter = np.multiply(t_filter_q, t_filter_vorz)
+    # t_filter = t_filter_q
+    # t_filter = t_filter_vorz
+
+    vorz_filter = np.multiply(g_filter, t_filter)
+
+    grid_vz_filtered = vorz_processing(grid_vz, vorz_filter)
+
+    # field_plot(window, grid_vz, grid_vz_filtered)
+    # field_plot(window, grid_vz, vorz_filter)
+    #----------------------------------------------------------------
+
+    #-----------vortices processing------------------------
+    s = ndimage.generate_binary_structure(2, 2)
+    vorz_l = ndimage.label(grid_vz_filtered, structure=s)
+    # print(vorz_l[1])
+    #-----------locations----------------
+    pixel_locations = ndimage.measurements.center_of_mass(
+        grid_vz_filtered, vorz_l[0], [x for x in range(1, vorz_l[1] + 1)])
+    # print(pixel_locations)
+    dx = (window[1] - window[0]) / resolution[0]
+    dy = (window[3] - window[2]) / resolution[1]
+    vorz_locations = []
+    for loci in pixel_locations:
+        v_locix = window[0] + loci[0] * dx
+        v_lociy = window[2] + loci[1] * dy
+        vorz_locations.append([v_locix, v_lociy])
+
+    vorz_locations = np.array(vorz_locations)
+    #-----------circulations----------------
+    pixel_sum = ndimage.sum(grid_vz_filtered, vorz_l[0],
+                            [x for x in range(1, vorz_l[1] + 1)])
+    circulations = []
+    for i in range(len(pixel_sum)):
+        circulations.append([pixel_sum[i] * dx * dy, i + 1])
+    circulations = np.array(circulations)
+
+    vortices = np.array(
+        [[loc[0], loc[1], vz[0], vz[1]]
+         for loc, vz in zip(vorz_locations, circulations)
+         if vz[0] >= threshold_circulation * np.amax(circulations[:, 0])
+         or vz[0] <= threshold_circulation * -np.amax(-circulations[:, 0])])
+
+    pvortices = np.array(
+        [[loc[0], loc[1], vz[0], vz[1]]
+         for loc, vz in zip(vorz_locations, circulations)
+         if vz[0] >= threshold_circulation * np.amax(circulations[:, 0])])
+    if len(pvortices) > 0:
+        pvortices = pvortices[pvortices[:, 2].argsort()[::-1]]
+
+    nvortices = np.array(
+        [[loc[0], loc[1], vz[0], vz[1]]
+         for loc, vz in zip(vorz_locations, circulations)
+         if vz[0] <= threshold_circulation * -np.amax(-circulations[:, 0])])
+    if len(nvortices) > 0:
+        nvortices = nvortices[nvortices[:, 2].argsort()]
+    #-------------------------------------------------------------------------
+    #-----organizing outputs------------
+    pcirculation_filter = vorz_filter * 0
+    ncirculation_filter = vorz_filter * 0
+
+    for vortexi in pvortices:
+        pcirculation_filter += (vorz_l[0] == vortexi[3]) * 1
+    for vortexi in nvortices:
+        ncirculation_filter += (vorz_l[0] == vortexi[3]) * 1
+    circulation_filter = pcirculation_filter + ncirculation_filter
+
+    pvz_filtered = vorz_processing(grid_vz, pcirculation_filter)
+    nvz_filtered = vorz_processing(grid_vz, ncirculation_filter)
+    vz_filtered = pvz_filtered + nvz_filtered
+
+    sorted_vortices = [pvortices, nvortices, vortices]
+    image_vortices = [
+        pcirculation_filter, ncirculation_filter, circulation_filter
+    ]
+    vz_field_filtered = [pvz_filtered, nvz_filtered, vz_filtered, grid_vz]
+    #-------------------------------------------------------------
+
+    return sorted_vortices, image_vortices, vz_field_filtered
