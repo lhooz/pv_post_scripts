c1d231f785f53ab02228fbb654e7b2f670cc2724
hao <hao.lee0019@gmail.com>
2020-08-25 02:51:24 +0100

    monior update in plot ref data path plus .csv ending

--- 3d_post/plot_results.py
+++ 3d_post/plot_results.py
@@ -4,8 +4,8 @@ from functions_plotting import read_cfd_data, read_ref_data, cf_plotter
 
 cwd = os.getcwd()
 cfd_data_list = ['sym', 'ad', 'dl']
-# ref_data_lst = ['ff_dickinson', 'adff_dickinson', 'dlff_dickinson']
-ref_data_lst = []
+ref_data_lst = ['sym_exp', 'sym_cfd1', 'sym_cfd2']
+# ref_data_lst = []
 
 cf_array = []
 for cfi in cfd_data_list:
@@ -16,15 +16,15 @@ for cfi in cfd_data_list:
 
 ref_array = []
 for refi in ref_data_lst:
-    ref_datai = os.path.join(cwd, refi)
+    ref_datai = os.path.join(cwd, refi + '.csv')
     ref_arrayi = read_ref_data(ref_datai)
 
     ref_array.append(ref_arrayi)
 
 data_array = [cf_array, ref_array]
 legends = [cfd_data_list, ref_data_lst]
-data_to_plot = ['sym', 'dl']
+data_to_plot = ['sym', 'sym_exp', 'sym_cfd1', 'sym_cfd2']
 # time_to_plot = 'all'
-time_to_plot = [1, 4]
+time_to_plot = [3, 4]
 
 cf_plotter(data_array, legends, data_to_plot, time_to_plot)
